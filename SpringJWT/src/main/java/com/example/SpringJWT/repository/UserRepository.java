a554c8b95fb84b666e4da04a778119031c13fd15
Juwon-Kiim <kimjuwon@gimjuwon-ui-MacBookAir.local>
2024-03-07 15:59:08 +0900

    create UserRepository

---
+++ SpringJWT/src/main/java/com/example/SpringJWT/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.example.SpringJWT.repository;
+
+import com.example.SpringJWT.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+}
