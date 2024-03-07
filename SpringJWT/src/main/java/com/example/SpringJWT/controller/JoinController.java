565185747ab598bf7f915bdd5b105f787bfbaf05
Juwon-Kiim <kimjuwon@gimjuwon-ui-MacBookAir.local>
2024-03-07 16:06:41 +0900

    create JoinController

---
+++ SpringJWT/src/main/java/com/example/SpringJWT/controller/JoinController.java
@@ -0,0 +1,25 @@
+package com.example.SpringJWT.controller;
+
+import com.example.SpringJWT.dto.JoinDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class JoinController {
+
+    private final JoinService joinService;
+
+    public JoinController(JoinService joinService) {
+        this.joinService = joinService;
+    }
+
+    @PostMapping("/join")
+    public String joinProcess(@RequestBody JoinDto joinDto){
+        joinService.join(joinDto);
+
+        return "ok";
+    }
+}
