dd28bbf229c2040a1a3d187c36e440953c75d610
Juwon-Kiim <kimjuwon@gimjuwon-ui-MacBookAir.local>
2024-03-07 16:08:05 +0900

    create JoinService

---
+++ SpringJWT/src/main/java/com/example/SpringJWT/service/JoinService.java
@@ -0,0 +1,11 @@
+package com.example.SpringJWT.service;
+
+import com.example.SpringJWT.dto.JoinDto;
+import com.example.SpringJWT.repository.UserRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class JoinService {
+    
+}
