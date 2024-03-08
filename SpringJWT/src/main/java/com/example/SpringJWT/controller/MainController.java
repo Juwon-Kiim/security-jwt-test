110c768ad460fc04febdeef649137235ec35c0dd
Juwon-Kiim <kimjuwon@gimjuwon-ui-MacBookAir.local>
2024-03-08 22:46:53 +0900

    check session data

--- SpringJWT/src/main/java/com/example/SpringJWT/controller/MainController.java
+++ SpringJWT/src/main/java/com/example/SpringJWT/controller/MainController.java
@@ -1,12 +1,24 @@
 package com.example.SpringJWT.controller;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+import java.util.Iterator;
 
 @RestController
 public class MainController {
     @GetMapping("/")
     public String mainPage(){
-        return "Main Controller";
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+
+        Collection<? extends GrantedAuthority> authorities = SecurityContextHolder.getContext().getAuthentication().getAuthorities();
+        Iterator<? extends GrantedAuthority> iterator = authorities.iterator();
+        GrantedAuthority auth = iterator.next();
+        String role = auth.getAuthority();
+
+        return "Main Controller " + username +" "+ role;
     }
 }
