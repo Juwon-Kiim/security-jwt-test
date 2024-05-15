9bd7a554e5381f8dd7b2acd8a7170ef6ec9b84af
Juwon-Kiim <kimjuwon@gimjuwon-ui-MacBookAir.local>
2024-05-15 17:20:39 +0900

    refresh rotate

--- SpringJWT/src/main/java/com/example/SpringJWT/controller/ReissueController.java
+++ SpringJWT/src/main/java/com/example/SpringJWT/controller/ReissueController.java
@@ -46,7 +46,17 @@ public class ReissueController {
         String role = jwtUtil.getRole(refresh);
 
         String newAccess = jwtUtil.createJwt("access", username, role, 600000L);
+        String newRefresh = jwtUtil.createJwt("refresh", username, role, 86400000L);
         response.setHeader("access", newAccess);
+        response.addCookie(createCookie("refresh", newRefresh));
         return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    private Cookie createCookie(String key, String value){
+        Cookie cookie = new Cookie(key, value);
+        cookie.setMaxAge(24*60*60);
+        cookie.setHttpOnly(true);
+
+        return cookie;
     }
 }
